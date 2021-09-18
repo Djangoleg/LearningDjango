f3e12755acbe06a94ff29756ebde12f2325eaaad
kroleg <kr.oleg@gmail.com>
2021-09-18 16:40:34 +0300

    Lesson 2

--- geekshop/products/urls.py
+++ geekshop/products/urls.py
@@ -13,13 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from products.views import index, products, test
+from .views import products
+
+app_name = 'products'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/', products, name='products'),
-    path('test/', test, name='test'),
+    path('', products, name='index'),
 ]
