c02e72b5b6451af62247071dfae4a03c3edaee42
kroleg <kr.oleg@gmail.com>
2021-09-19 00:24:48 +0300

    Add models

--- geekshop/products/models.py
+++ geekshop/products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+'''
+python manage.py makemigrations
+Создание модели в БД.
+python manage.py migrate
+
+В случае чего, удаляем миграции migrations (0001..) и саму БД.
+'''
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(blank=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    image = models.ImageField(upload_to='product_images', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
