6b4bc2ce5b7bd417207c61bf34e05d4f5862d57d
kroleg <kr.oleg@gmail.com>
2021-09-19 17:09:16 +0300

    Add model to admin

--- geekshop/products/models.py
+++ geekshop/products/models.py
@@ -13,6 +13,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
@@ -21,3 +24,9 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} | {self.category.name}"
+
+
+
