2aaf4bbec8fdc1f3664fa151de9e22a13df0fe8b
kroleg <kr.oleg@gmail.com>
2021-09-24 02:27:36 +0300

    edit users app

--- geekshop/users/views.py
+++ geekshop/users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def login(request):
+    content = {
+        'title': 'GeekShop - Авторизация'
+    }
+    return render(request, 'users/login.html', content)
+
+
+def register(request):
+    content = {
+        'title': 'GeekShop - Регистрация'
+    }
+    return render(request, 'users/register.html', content)
