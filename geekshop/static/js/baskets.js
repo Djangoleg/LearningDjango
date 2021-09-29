e1442958063ce8d5bce8af96684873d75cfef0d9
kroleg <kr.oleg@gmail.com>
2021-09-29 17:43:42 +0300

    Edit baskets script

--- geekshop/static/js/baskets.js
+++ geekshop/static/js/baskets.js
@@ -2,8 +2,6 @@ window.onload = () => {
 
     $('.basket_list').on('click', 'input[type="number"]', (e) => {
         let t_href = e.target;
-        console.log(t_href.name);
-        console.log(t_href.value);
 
         $.ajax({
             url: '../../baskets/edit/' + t_href.name + '/' + t_href.value + '/',
