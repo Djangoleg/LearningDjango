9bd403526ed3b74ec8a19c26b9e69abfacc391ec
kroleg <kr.oleg@gmail.com>
2021-10-07 15:58:07 +0300

    Add products filter and paginator

--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import ProductCategory, Product
 
@@ -17,12 +18,24 @@ def index(request):
     return render(request, 'products/index.html', content)
 
 
-def products(request):
+def products(request, category_id=None, page_id=1):
+    products = Product.objects.filter(
+        category_id=category_id) if category_id is not None else Product.objects.all()
 
-    content = {"title": "GeekShop",
-               "currency": "руб",
-               "categories": ProductCategory.objects.all(),
-               "products": Product.objects.all()}
+    paginator = Paginator(products, per_page=3)
+
+    try:
+        products_paginator = paginator.page(page_id)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+
+    content = {
+        "title": "Каталог",
+        "currency": "руб",
+        "categories": ProductCategory.objects.all(),
+        "products": products_paginator,
+    }
 
     return render(request, 'products/products.html', content)
-
