435e9e346aec2308256a9f3b85906ace780d3bba
kroleg <kr.oleg@gmail.com>
2021-10-09 20:32:40 +0300

    Edit basked view. Rewrite to class

--- geekshop/baskets/views.py
+++ geekshop/baskets/views.py
@@ -1,49 +1,77 @@
 from django.http import JsonResponse
-from django.shortcuts import render, HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+
 from django.template.loader import render_to_string
+from django.urls import reverse_lazy
+
+from django.views.generic import DeleteView, CreateView, UpdateView
 
 from baskets.models import Basket
+from geekshop.mixin import UserDispatchMixin
 from products.models import Product
 
 
-@login_required
-def basket_add(request, product_id):
-    product = Product.objects.get(id=product_id)
-    baskets = Basket.objects.filter(user=request.user, product=product)
-    if not baskets.exists():
-        Basket.objects.create(user=request.user, product=product, quantity=1)
-    else:
-        basket = baskets.first()
-        basket.quantity += 1
-        basket.save()
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+class BasketCreateView(CreateView, UserDispatchMixin):
+    model = Basket
+    fields = ['product']
+    template_name = 'products/products.html'
+    success_url = reverse_lazy('products:index')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        if 'product_id' in kwargs:
+            product_id = kwargs['product_id']
+            if product_id:
+                product = Product.objects.get(id=product_id)
+                baskets = Basket.objects.filter(user=request.user, product=product)
+                if not baskets.exists():
+                    Basket.objects.create(user=request.user, product=product, quantity=1)
+                else:
+                    basket = baskets.first()
+                    basket.quantity += 1
+                    basket.save()
+
+        return redirect(self.success_url)
 
 
-@login_required
-def basket_remove(request, basket_id):
-    baskets = Basket.objects.filter(id=basket_id)
-    if baskets.exists():
-        baskets.delete()
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+class BasketDeleteView(DeleteView, UserDispatchMixin):
+    model = Basket
+    success_url = reverse_lazy('users:profile')
 
 
-@login_required
-def basket_edit(request, basket_id, quantity):
-    if request.is_ajax():
-        baskets = Basket.objects.filter(id=basket_id)
-        if baskets.exists():
-            basket = baskets.first()
-            if quantity > 0:
-                basket.quantity = quantity
-                basket.save()
-            else:
-                basket.delete()
+class BasketUpdateView(UpdateView, UserDispatchMixin):
+    model = Basket
+    fields = ['product']
+    template_name = 'users/profile.html'
+    success_url = reverse_lazy('users:profile')
+    pk_url_kwarg = 'basket_id'
 
-        baskets = Basket.objects.filter(user=request.user)
-        context = {
-            'baskets': baskets,
-        }
-        result = render_to_string('baskets/baskets.html', context)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        return JsonResponse({'result': result})
+    def get_context_data(self, *args, **kwargs):
+        context = super(BasketUpdateView, self).get_context_data(**kwargs)
+        context['baskets'] = Basket.objects.filter(user=self.request.user)
+        return context
+
+    def get(self, request, *args, **kwargs):
+        super(BasketUpdateView, self).get(request, *args, **kwargs)
+        if request.is_ajax():
+            basket_id = kwargs[self.pk_url_kwarg]
+            quantity = kwargs['quantity']
+            baskets = Basket.objects.filter(id=basket_id)
+            if baskets.exists():
+                basket = baskets.first()
+                if quantity > 0:
+                    basket.quantity = quantity
+                    basket.save()
+                else:
+                    basket.delete()
+
+            result = render_to_string('baskets/baskets.html', self.get_context_data(*args, **kwargs))
+
+            return JsonResponse({'result': result})
+
+        return redirect(self.success_url)
