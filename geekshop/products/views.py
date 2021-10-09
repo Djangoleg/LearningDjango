0908937cd889faa68de39bbcff9f1df10f7434f3
kroleg <kr.oleg@gmail.com>
2021-10-09 21:22:36 +0300

    Rewrite products to class

--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -2,7 +2,9 @@ import datetime
 import json
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
+from geekshop.mixin import UserDispatchMixin
 from .models import ProductCategory, Product
 
 
@@ -18,24 +20,38 @@ def index(request):
     return render(request, 'products/index.html', content)
 
 
-def products(request, category_id=None, page_id=1):
-    products = Product.objects.filter(
-        category_id=category_id) if category_id is not None else Product.objects.all()
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/products.html'
+    context_object_name = 'products'
 
-    paginator = Paginator(products, per_page=3)
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+        context['title'] = 'Каталог'
+        context['currency'] = "руб"
+        context['categories'] = ProductCategory.objects.all()
 
-    try:
-        products_paginator = paginator.page(page_id)
-    except PageNotAnInteger:
-        products_paginator = paginator.page(1)
-    except EmptyPage:
-        products_paginator = paginator.page(paginator.num_pages)
+        category_id = None
+        page_id = 1
 
-    content = {
-        "title": "Каталог",
-        "currency": "руб",
-        "categories": ProductCategory.objects.all(),
-        "products": products_paginator,
-    }
+        if 'category_id' in self.kwargs:
+            category_id = self.kwargs['category_id']
 
-    return render(request, 'products/products.html', content)
+        if 'page_id' in self.kwargs:
+            page_id = self.kwargs['page_id']
+
+        products = Product.objects.filter(
+            category_id=category_id) if category_id is not None else Product.objects.all()
+
+        paginator = Paginator(products, per_page=3)
+
+        try:
+            products_paginator = paginator.page(page_id)
+        except PageNotAnInteger:
+            products_paginator = paginator.page(1)
+        except EmptyPage:
+            products_paginator = paginator.page(paginator.num_pages)
+
+        context['products'] = products_paginator
+
+        return context
