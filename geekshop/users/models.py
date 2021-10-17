5cc1dc93d81f28532e3aeaf7387ec74c3fe265de
kroleg <kr.oleg@gmail.com>
2021-10-18 00:26:24 +0300

    Add UserProfileEditForm

--- geekshop/users/models.py
+++ geekshop/users/models.py
@@ -2,6 +2,8 @@ from datetime import timedelta
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.timezone import now
 
 NULL_INSTALL = {'null': True, 'blank': True}
@@ -19,3 +21,27 @@ class User(AbstractUser):
             return False
         else:
             return True
+
+
+class UserProfile(models.Model):
+
+    MALE = 'M'
+    FEMALE = 'F'
+    GENDER = (
+        (MALE, 'М'),
+        (FEMALE, 'Ж'),
+    )
+
+    user = models.OneToOneField(User, unique=True, null=False, db_index=True, on_delete=models.CASCADE)
+    tagLine = models.CharField(verbose_name='теги', max_length=128, blank=True)
+    about = models.TextField(verbose_name='о себе', blank=True, null=True)
+    gender = models.CharField(verbose_name='пол', choices=GENDER, blank=True, max_length=5)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.shopuserprofile.save()
