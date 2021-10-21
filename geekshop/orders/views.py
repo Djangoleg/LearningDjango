e0f5f2be22b77e223f8187e44e93b2b470a6535e
kroleg <kr.oleg@gmail.com>
2021-10-22 00:18:09 +0300

    Edit order

--- geekshop/orders/views.py
+++ geekshop/orders/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django import forms
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+
+from orders.models import Order, OrderItem
+
+
+class OrderList(ListView):
+    pass
+
+
+class OrderCreate(CreateView):
+    pass
+
+
+class OrderUpdate(UpdateView):
+    pass
+
+
+class OrderDelete(DeleteView):
+    pass
+
+
+class OrderDetail(DetailView):
+    pass
+
+
+def order_forming_complete(request, pk):
+    pass
