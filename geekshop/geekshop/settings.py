c7631cf07bd753051b03f09264b9248adb012580
kroleg <kr.oleg@gmail.com>
2021-10-27 00:51:09 +0300

    edit settings

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -149,7 +149,7 @@ LOGIN_REDIRECT_URL = '/'
 
 DOMAIN_NAME = os.getenv('DOMAIN_NAME')
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = True if os.getenv('EMAIL_USE_SSL') == 'True' else False
