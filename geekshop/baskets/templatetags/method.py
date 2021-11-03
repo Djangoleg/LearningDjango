63fce11fad58d980d7196716c2b46b4d48a4d8dc
kroleg <kr.oleg@gmail.com>
2021-11-03 22:05:57 +0300

    Basket optimization. Add cached property

--- geekshop/baskets/templatetags/method.py
+++ geekshop/baskets/templatetags/method.py
@@ -6,9 +6,13 @@ register = template.Library()
 
 @register.filter(name='total_quantity')
 def total_quantity(value, user):
-    return Basket.total_quantity(user)
+    basket = Basket()
+    basket.user = user
+    return basket.total_quantity()
 
 
 @register.filter(name='total_sum')
 def total_sum(value, user):
-    return Basket.total_sum(user)
+    basket = Basket()
+    basket.user = user
+    return basket.total_sum()
