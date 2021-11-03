52f118a57e90e8b2ce699367ee78799d48687553
kroleg <kr.oleg@gmail.com>
2021-11-04 02:49:56 +0300

    Edit settings

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-4h4!^xm8b2djh=t-6z5)-cn^-$du0btt3=f*r4ms!+37h=)_1_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
